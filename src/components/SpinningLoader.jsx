8b6842f363a48bbe5b4cbc6c3966b961099cd903
carsten.greve@gmail.com <carsten.greve@gmail.com>
2026-02-07 18:14:32 +0100

    Not used

--- src/components/SpinningLoader.jsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from 'lucide-react';
-
-function SpinningLoader () {
-  return (
-    <Loader className="h-10 w-10 text-blue-500 animate-spin" />
-  );
-};
-
-export default SpinningLoader
